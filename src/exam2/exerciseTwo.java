9cb28f889347782bd26dbfe35e9a84eafae23afd
Zolbayar <zzolbaya@gmail.com>
2023-05-03 12:08:08 +0800

    exercisetwo nemlee

--- src/exam2/exerciseTwo.java
+++ src/exam2/exerciseTwo.java
@@ -3,7 +3,7 @@ package exam2;
 public class exerciseTwo extends Thread {
 
 	  public void run() {
-			System.out.print("Ursgal ajillaj bain");
+			System.out.print("Ursgal ajillaj bain.");
 		}
 		public static void main(String[] args) {
 			exerciseTwo thread1 = new exerciseTwo();
